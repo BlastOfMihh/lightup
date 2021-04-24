b10b569ee963e7849d21886aff28d801d991df9b
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-04-24 19:47:07 +0300

    latest

--- lightup/ScenesNScripts/Spell/Spell.gd
+++ lightup/ScenesNScripts/Spell/Spell.gd
@@ -14,5 +14,8 @@ func setup(which:String):
 	# type=spells[which]
 	pass
 
+func _physics_process(delta):
+	rotation = PI+(get_global_position()-get_global_mouse_position()).angle()
+
 # func is_ready()->bool:
 # 	return cspell.is_ready()
