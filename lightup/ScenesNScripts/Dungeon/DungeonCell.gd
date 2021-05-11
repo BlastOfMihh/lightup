7bc88b4deaa17734c36a0f927a5c6a22d1eceaa7
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-05-11 18:48:56 +0300

    asd

--- lightup/ScenesNScripts/Dungeon/DungeonCell.gd
+++ lightup/ScenesNScripts/Dungeon/DungeonCell.gd
@@ -4,7 +4,9 @@ class_name DungeonCell
 
 var csize=Vector2()
 
-func setup(sz, pos)->void:
+var type="none"
+
+func setup_pos(sz, pos)->void:
 	csize=sz
 	position=pos
 	print("new thing")
@@ -12,3 +14,10 @@ func setup(sz, pos)->void:
 	var sp=Sprite.new()
 	sp.texture=load("res://Assets/godot_logo.png")
 	add_child(sp)
+
+# func setup_type(s):
+# 	match s:
+# 		"room":
+# 			pass
+# 		"road":
+# 			pass
