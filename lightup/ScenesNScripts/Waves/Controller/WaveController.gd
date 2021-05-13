4aa204604f4383efa01e91a688e80c9cb630512e
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-05-13 17:32:37 +0300

    DUNGEON IMOROV

--- lightup/ScenesNScripts/Waves/Controller/WaveController.gd
+++ lightup/ScenesNScripts/Waves/Controller/WaveController.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
 onready var waves=$Waves
+onready var doors=$Doors
 export var active:=false
+
+signal close_doors()
+signal open_doors()
+
+func _ready():
+	for d in doors.get_children():
+		connect("close_doors",d,"close")
+		connect("open_doors",d,"open")
 
 func handle_waves():
 	if waves.get_child_count()>0 and active:
@@ -10,7 +19,12 @@ func handle_waves():
 			cwave.spawn()
 
 func _process(delta):
-	handle_waves()
+	if active:
+		handle_waves()
+	if waves.get_child_count()==0:
+		emit_signal("open_doors")
+		queue_free()
 
 func _on_AreaRange_body_entered(body):
+	emit_signal("close_doors")
 	active=true
--- lightup/ScenesNScripts/Waves/Wave/Door.gd
+++ lightup/ScenesNScripts/Waves/Wave/Door.gd
@@ -1,16 +1,11 @@
-extends KinematicBody2D
+extends Node2D
 
+onready var tilemap=$TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	open()
+func close():
+	self.add_child(tilemap)
+	
+func open():
+	tilemap.get_parent().remove_child(tilemap)
