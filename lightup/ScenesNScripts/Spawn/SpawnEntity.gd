01211f2a51d10f7bb8b4720321e43d00dc797552
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-05-13 15:12:02 +0300

    asd

--- lightup/ScenesNScripts/Spawn/SpawnEntity.gd
+++ lightup/ScenesNScripts/Spawn/SpawnEntity.gd
@@ -1,3 +1,15 @@
 extends Node2D
 
 export(PackedScene) var entity
+
+onready var sp=$AnimatedSprite
+
+func _ready():
+	sp.play("default")
+
+func _on_AnimatedSprite_animation_finished():
+	var ent=entity.instance()
+	ent.position=position
+	get_parent().add_child(ent)
+	queue_free()
+	
