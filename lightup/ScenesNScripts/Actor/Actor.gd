5a6f997c096ada9d75dd2567aa776c7864fadae1
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-05-13 22:25:15 +0300

    done for today

--- lightup/ScenesNScripts/Actor/Actor.gd
+++ lightup/ScenesNScripts/Actor/Actor.gd
@@ -1,48 +1,45 @@
 
 
 
-##
-##
 
+extends MBody
 
-		extends MBody
+var dir=Vector2.ZERO
+var speed=200
 
-		var dir=Vector2.ZERO
-		var speed=200
+onready var sh=$SwordHandler
+onready var sr=$SwordRooster
+onready var sm=$StateMachine
+onready var ap=$AnimationPlayer
+onready var sp=$Visuals/AnimatedSprite
+onready var invs=[$UI/InventoryContainer, $UI/InventoryContainer2]
 
-		onready var sh=$SwordHandler
-		onready var sr=$SwordRooster
-		onready var sm=$StateMachine
-		onready var ap=$AnimationPlayer
-		onready var sp=$Visuals/AnimatedSprite
-		onready var invs=[$UI/InventoryContainer, $UI/InventoryContainer2]
+func _ready():
+	Globals.actor=self
 
-		func _ready():
-			Globals.actor=self
+func update_dir():
+	dir.x = -Input.get_action_strength("move_left")+Input.get_action_strength("move_right")
+	dir.y = Input.get_action_strength("move_down")-Input.get_action_strength("move_up")
 
-		func update_dir():
-			dir.x = -Input.get_action_strength("move_left")+Input.get_action_strength("move_right")
-			dir.y = Input.get_action_strength("move_down")-Input.get_action_strength("move_up")
+func update_velos(delta):
+	velos=dir*speed
 
-		func update_velos(delta):
-			velos=dir*speed
+func _physics_process(delta):
+	# if Input.is_key_pressed(KEY_T):
+	# 	sm.request_state("Dash")
+	update_dir()
+	# update_velos(delta)
+	apply_velos(delta)
 
-		func _physics_process(delta):
-			# if Input.is_key_pressed(KEY_T):
-			# 	sm.request_state("Dash")
-			update_dir()
-			# update_velos(delta)
-			apply_velos(delta)
+func hurt():
+	sm.request_state("Stagger")
+	# print("hurrrrt")
 
-		func hurt():
-			sm.request_state("Stagger")
-			# print("hurrrrt")
-
-		func get_spell_number()->int:
-			for i in range(0,10):
-				var ss="spell"+str(i)
-				# ss.push_back('0')
-				if Input.is_action_pressed(ss):
-					return i
-			return -1
-		# pass
+func get_spell_number()->int:
+	for i in range(0,10):
+		var ss="spell"+str(i)
+		# ss.push_back('0')
+		if Input.is_action_pressed(ss):
+			return i
+	return -1
+# pass
