40667c4b6132c4702c7bd03d172fa6d193321f57
BlastOfMihh <42068395+BlastOfMihh@users.noreply.github.com>
2021-05-21 01:49:13 +0300

    asd
    as

--- lightup/ScenesNScripts/Actor/States/Stand.gd
+++ lightup/ScenesNScripts/Actor/States/Stand.gd
@@ -5,7 +5,21 @@ func _ready():
 	removing_states=["Walk","Stagger","Dash"]
 	necessary_states=[]
 
+export var dash_init_speed=300
+export var dash_friction=950
+
 func get_transition():
+	if Input.is_key_pressed(SPKEY):
+		var dash=sm.request_state("Dash")
+		if dash:
+			print("hi dash")
+			var v=(pr.global_position-pr.get_global_mouse_position()).normalized()
+			dash.dash_vector=v
+#			dash.dash_vector=Vector2(cos(global_rotation), sin(global_rotation))*dash_init_speed
+			dash.friction=dash_friction
+			dash.enter_state([])
+
+		
 	if pr.velos!=Vector2.ZERO:
 		return "Walk"
 	return null
